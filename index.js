cd7b605d56373f0b5caf8b4b109307ab73c14438
tanimtonni12 <17103062iubat@gmail.com>
2022-05-08 16:37:53 +0600

    delete post update done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { get } = require('express/lib/response');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -20,7 +21,10 @@ async function run() {
         const productCollection = client.db('wareHouseManagement').collection('product');
         //get data
         app.get('/product', async (req, res) => {
-            console.log(req.body)
+
+            // console.log(req.body)
+
+
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
@@ -28,12 +32,62 @@ async function run() {
 
 
         });
+        app.get('/product/my', async (req, res) => {
+            const email = req.query.email;
+            console.log(email);
+            const query = { email: email };
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+
+        });
+
+
+        app.put('/product/:id/delivered', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            const updateDoc = {
+                $set: {
+                    quantity: product.quantity - 1,
+                    sold: product.sold + 1
+                },
+            };
+            const result = await productCollection.updateOne(query, updateDoc);
+            res.send(result);
+
+        });
+
+
+        //restock
+        app.put('/product/:id/restock', async (req, res) => {
+            const id = req.params.id;
+            const { quantity } = req.body;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            const updateDoc = {
+                $set: {
+                    quantity: parseInt(product.quantity) + parseInt(quantity)
+                },
+            };
+            const result = await productCollection.updateOne(query, updateDoc);
+            res.send(result);
+
+        });
+
+
+
+
+
+
         //post data add
         app.post('/product', async (req, res) => {
             const newProduct = req.body;
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
         });
+
 
         //find one
         app.get('/product/:id', async (req, res) => {
@@ -46,6 +100,25 @@ async function run() {
 
         });
 
+        app.put('/product/:id', async (res, req) => {
+            const id = req.params.id;
+            const updateStock = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: updateStock
+
+
+            };
+            const result = await productCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+
+        });
+
+
+
+
+
         //delete
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
@@ -54,12 +127,15 @@ async function run() {
             res.send(result);
 
 
-        })
+        });
+
+
+
 
 
     }
     finally {
-        console.log('error')
+
 
     }
 
